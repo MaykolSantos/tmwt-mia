cf40035c3d16d6c0cf1d76f34cab251ca5ca1e79
Maykol Santos <maykol.silva.santos@winning.pt>
2024-05-21 11:47:02 +0100

    Changes and improvements. Remooved some code and it wasn't tested yet. As soon as it is tested, i'll merge this to main

--- tmwt/app/src/main/java/com/dissertation/tmwt/BLEConnectionTask.java
+++ tmwt/app/src/main/java/com/dissertation/tmwt/BLEConnectionTask.java
@@ -1,28 +1,13 @@
 package com.dissertation.tmwt;
 
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
-import android.content.ContentValues;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-
-import com.st.blue_sdk.board_catalog.models.BoardFirmware;
-import com.st.blue_sdk.bt.advertise.BleAdvertiseInfo;
 import com.st.blue_sdk.features.Feature;
 import com.st.blue_sdk.features.FeatureUpdate;
 import com.st.blue_sdk.features.acceleration.Acceleration;
@@ -33,83 +18,55 @@ import com.st.blue_sdk.features.magnetometer.Magnetometer;
 import com.st.blue_sdk.features.magnetometer.MagnetometerInfo;
 import com.st.blue_sdk.features.temperature.Temperature;
 import com.st.blue_sdk.features.temperature.TemperatureInfo;
-import com.st.blue_sdk.logger.CsvFileLogger;
-import com.st.blue_sdk.models.ConnectionStatus;
-import com.st.blue_sdk.models.Node;
-import com.st.blue_sdk.models.RssiData;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
 public class BLEConnectionTask implements Runnable {
-    private static final int REQUEST_BLUETOOTH_CONNECT_PERMISSION = 1;
     private final static UUID SERVICE_UUID = UUID.fromString("00000000-0001-11e1-9ab4-0002a5d5c51b");
     private BluetoothDevice device;
     private Context context;
     private BluetoothGatt bluetoothGatt;
-    private BluetoothGattService service;
-    private String sensorDataString;
+    private int numberOfCurrentProcesses;
     private final static String ACCEL_CHARACTERISTIC_NAME = "Accelerometer";
     private final static String MAGN_CHARACTERISTIC_NAME = "Magnetometer";
     private final static String TEMP_CHARACTERISTIC_NAME = "Temperature";
     private final static String GYRO_CHARACTERISTIC_NAME = "Gyroscope";
-    public CsvFileLogger csvFileLogger;
     public UUID characteristicUUID;
     public String characteristicName;
     private String fileName;
     private TaskMonitor taskMonitor;
 
-    public BLEConnectionTask(Context context, BluetoothDevice device, String fileName, UUID characteristicUUID, String characteristicName, TaskMonitor taskMonitor) {
+    public BLEConnectionTask(Context context, BluetoothDevice device, String fileName, UUID characteristicUUID, String characteristicName, TaskMonitor taskMonitor, int numberOfCurrentProcesses) {
         this.context = context;
         this.device = device;
         this.fileName = fileName;
         this.characteristicUUID = characteristicUUID;
         this.characteristicName = characteristicName;
         this.taskMonitor = taskMonitor;
-
-        // Construct the full path
-        File logDirectory = new File(context.getExternalFilesDir(null) + File.separator + "10MWT" + File.separator + fileName);
-        if (!logDirectory.exists()) {
-            logDirectory.mkdirs(); // Ensure the directory structure exists
-        }
-
-        Log.d("Path", logDirectory.getAbsolutePath());
-
-        // Initialize CsvFileLogger with the full path
-        this.csvFileLogger = new CsvFileLogger(logDirectory.getAbsolutePath());
-        // Assuming there's a method to enable logging
-        this.csvFileLogger.setEnabled(true);
-
+        this.numberOfCurrentProcesses = numberOfCurrentProcesses;
     }
 
     @Override
     public void run() {
         try {
-            taskMonitor.incrementTasks();
             connectToDevice(device);
-
-            // Example of a task doing periodic work
             while (!Thread.currentThread().isInterrupted()) {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt(); // Preserve interrupt status
-                    break; // Exit the loop
+                    Thread.currentThread().interrupt();
+                    break;
                 }
             }
         } catch (Exception e) {
             Thread.currentThread().interrupt();
         } finally {
-            taskMonitor.decrementTasks();
-            disconnect(); // Ensure resources are cleaned up properly
+            disconnect();
         }
     }
 
@@ -118,11 +75,10 @@ public class BLEConnectionTask implements Runnable {
             @Override
             public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
                 if (newState == BluetoothProfile.STATE_CONNECTED) {
-                    // Connected to the GATT server on the device
-//                    Log.d("BLE", "Connected to GATT server.");
                     gatt.discoverServices();
+//                    Log.d("BLE", "Connected to GATT server.");
+                    taskMonitor.incrementTasks();
                 } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                    // Disconnected from the GATT server
 //                    Log.d("BLE", "Disconnected from GATT server.");
                 }
             }
@@ -162,10 +118,7 @@ public class BLEConnectionTask implements Runnable {
                 if (characteristicUUID.equals(currentCharacteristicUUID)) {
                     byte[] data = characteristic.getValue();
 
-                    // Retrieve the device address or name from the gatt instance
                     String deviceAddress = gatt.getDevice().getAddress();
-
-                    // Assume timestamp is now; in real applications, this might come from the device or be more accurately tracked
                     long timestamp = System.currentTimeMillis();
 
                     if (characteristicName.equals("Accelerometer")) {
@@ -177,7 +130,6 @@ public class BLEConnectionTask implements Runnable {
                     } else if (characteristicName.equals("Gyroscope")) {
                         getGyroscopeData(data, deviceAddress, timestamp);
                     } else {
-                        // Optionally handle unknown characteristics or log an error
                         System.out.println("Unknown characteristic: " + characteristicName);
                     }
                 }
@@ -192,9 +144,7 @@ public class BLEConnectionTask implements Runnable {
             BluetoothGattCharacteristic characteristic = service.getCharacteristic(characteristicUUID);
 
             if (characteristic != null) {
-                // Check if the characteristic supports notifications
                 if ((characteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_NOTIFY) != 0) {
-                    // The characteristic supports notifications, proceed to enable them
                     gatt.setCharacteristicNotification(characteristic, true);
 
                     // Find the notification descriptor and write the value to enable notifications
@@ -226,35 +176,64 @@ public class BLEConnectionTask implements Runnable {
         if (bluetoothGatt != null) {
             bluetoothGatt.disconnect();
             bluetoothGatt.close();
+            taskMonitor.decrementTasks();
         }
     }
 
     private void writeToCsv(String data, String sensorType) {
-        File logDirectory = new File(context.getExternalFilesDir(null) + File.separator + "10MWT" + File.separator + fileName);
-        if (!logDirectory.exists()) {
-            boolean isDirectoryCreated = logDirectory.mkdirs();
-//            Log.d("BLE", "Directory creation " + (isDirectoryCreated ? "successful" : "failed") + " at " + logDirectory.getAbsolutePath());
-        } else {
-//            Log.d("BLE", "Directory already exists.");
-        }
+        if (this.verifyProcessCount()) {
+            // Define the directory path for the logs
+            File logDirectory = new File(context.getExternalFilesDir(null) + File.separator + "10MWT" + File.separator + fileName);
 
-        File csvFile = new File(logDirectory, device.getName().toString().trim() + "_" + sensorType + ".csv");
-        if (!csvFile.exists()) {
-            try {
-                boolean isFileCreated = csvFile.createNewFile();
-//                Log.d("BLE", "File creation " + (isFileCreated ? "successful" : "failed") + " at " + csvFile.getAbsolutePath());
-            } catch (IOException e) {
-//                Log.e("BLE", "Error creating CSV file", e);
+            // Create directory if it does not exist
+            if (!logDirectory.exists()) {
+                boolean isDirectoryCreated = logDirectory.mkdirs();
+                // Log.d("BLE", "Directory creation " + (isDirectoryCreated ? "successful" : "failed") + " at " + logDirectory.getAbsolutePath());
+            } else {
+                // Log.d("BLE", "Directory already exists.");
             }
-        } else {
-//            Log.d("BLE", "File already exists.");
-        }
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFile, true))) {
-            bw.write(data);
-            bw.newLine();
-        } catch (IOException e) {
-//            Log.e("BLE", "Error writing to CSV file", e);
+            // Define the CSV file path
+            File csvFile = new File(logDirectory, device.getName().toString().trim() + "_" + sensorType + ".csv");
+
+            boolean isNewFile = false;
+            if (!csvFile.exists()) {
+                try {
+                    isNewFile = csvFile.createNewFile();
+                    // Log.d("BLE", "File creation " + (isNewFile ? "successful" : "failed") + " at " + csvFile.getAbsolutePath());
+                } catch (IOException e) {
+                    // Log.e("BLE", "Error creating CSV file", e);
+                }
+            } else {
+                // Log.d("BLE", "File already exists.");
+            }
+
+            // Add header
+            String header = "";
+            switch (sensorType) {
+                case "Accelerometer":
+                case "Gyroscope":
+                case "Magnetometer":
+                    header = "timestamp,X,Y,Z";
+                    break;
+                case "Temperature":
+                    header = "timestamp,temp";
+                    break;
+                default:
+                    // Log.d("BLE", "Unknown sensor type.");
+                    break;
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFile, true))) {
+                if (isNewFile && !header.isEmpty()) {
+                    bw.write(header);
+                    bw.newLine();
+                }
+                bw.write(data);
+                bw.newLine();
+            } catch (IOException e) {
+                // Log.e("BLE", "Error writing to CSV file", e);
+            }
         }
     }
 
@@ -263,7 +242,7 @@ public class BLEConnectionTask implements Runnable {
         FeatureUpdate<GyroscopeInfo> update = gyroscopeFeature.extractData(timestamp, rawData, 2);
 
         GyroscopeInfo gyroscopeInfo = update.getData();
-        String csvData = String.format(Locale.getDefault(), "%d, %f, %f, %f", timestamp, gyroscopeInfo.getX().getValue(), gyroscopeInfo.getY().getValue(), gyroscopeInfo.getZ().getValue());
+        String csvData = String.format(Locale.getDefault(), "%d,%f,%f,%f", timestamp, gyroscopeInfo.getX().getValue(), gyroscopeInfo.getY().getValue(), gyroscopeInfo.getZ().getValue());
 
         writeToCsv(csvData, characteristicName);
 
@@ -278,7 +257,7 @@ public class BLEConnectionTask implements Runnable {
         FeatureUpdate<TemperatureInfo> update = temperatureFeature.extractData(timestamp, rawData, 8);
 
         TemperatureInfo temperatureInfo = update.getData();
-        String csvData = String.format(Locale.getDefault(), "%d, %f", timestamp, temperatureInfo.getTemperature().getValue());
+        String csvData = String.format(Locale.getDefault(), "%d,%f", timestamp, temperatureInfo.getTemperature().getValue());
 
         writeToCsv(csvData, characteristicName);
 
@@ -291,7 +270,7 @@ public class BLEConnectionTask implements Runnable {
         FeatureUpdate<MagnetometerInfo> update = magnetometerFeature.extractData(timestamp, rawData, 2);
 
         MagnetometerInfo magnetometerInfo = update.getData();
-        String csvData = String.format(Locale.getDefault(), "%d, %f, %f, %f", timestamp, magnetometerInfo.getX().getValue(), magnetometerInfo.getY().getValue(), magnetometerInfo.getZ().getValue());
+        String csvData = String.format(Locale.getDefault(), "%d,%f,%f,%f", timestamp, magnetometerInfo.getX().getValue(), magnetometerInfo.getY().getValue(), magnetometerInfo.getZ().getValue());
 
         writeToCsv(csvData, characteristicName);
 
@@ -311,7 +290,7 @@ public class BLEConnectionTask implements Runnable {
         AccelerationInfo accelerationInfo = update.getData();
 
         // Format the acceleration data as a CSV string
-        String csvData = String.format(Locale.getDefault(), "%d, %f, %f, %f", timestamp, accelerationInfo.getX().getValue(), accelerationInfo.getY().getValue(), accelerationInfo.getZ().getValue());
+        String csvData = String.format(Locale.getDefault(), "%d,%f,%f,%f", timestamp, accelerationInfo.getX().getValue(), accelerationInfo.getY().getValue(), accelerationInfo.getZ().getValue());
 
         // Write the CSV data to file
         writeToCsv(csvData, characteristicName);
@@ -320,5 +299,11 @@ public class BLEConnectionTask implements Runnable {
 //        Log.d("BLE", "Device: " + deviceAddress + " Acceleration X: " + accelerationInfo.getX().getValue() + " " + accelerationInfo.getX().getUnit());
 //        Log.d("BLE", "Device: " + deviceAddress + " Acceleration Y: " + accelerationInfo.getY().getValue() + " " + accelerationInfo.getY().getUnit());
 //        Log.d("BLE", "Device: " + deviceAddress + " Acceleration Z: " + accelerationInfo.getZ().getValue() + " " + accelerationInfo.getZ().getUnit());
+    }
+
+    private boolean verifyProcessCount() {
+        var numberOfRunningProcesses = taskMonitor.getNumberOfTasks();
+
+        return numberOfRunningProcesses == numberOfCurrentProcesses && numberOfCurrentProcesses != 0;
     }
 }
